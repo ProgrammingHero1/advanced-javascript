d0e927dd96f544217fd21716b27a52dbffb3cdb5
That JS Dude <khan4019@gmail.com>
2020-01-02 18:35:21 -0800

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return 
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const premik = {name:"smart dude", phone:458421};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9]
+console.log(ages[11]);
